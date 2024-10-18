daaa62073f8218d278368a172569bfe5bf8759af
nikittch <nikita2006bondarienko@gmail.com>
2024-10-18 17:46:40 +0300

    1234

--- лр2.java
+++ лр2.java
@@ -7,7 +7,6 @@ public class FactorialCalculator {
         System.out.print("Введите число для вычисления факториала: ");
         int number = scanner.nextInt();
         
-        // Проверка на отрицательные числа
         if (number < 0) {
             System.out.println("Факториал отрицательного числа не определен.");
         } else {
@@ -15,8 +14,8 @@ public class FactorialCalculator {
             int i = 1;
             
             while (i <= number) {
-                factorial *= i; // Умножаем текущее значение факториала на i
-                i++; // Увеличиваем i на 1
+                factorial *= i; 
+                i++;
             }
             
             System.out.println("Факториал числа " + number + " равен " + factorial);
